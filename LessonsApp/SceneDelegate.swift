18d7c619fd3b611f4ee76c38309d7d98ff887e48
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-04-26 18:12:23 +0300

    Add navigationController inside SceneDelegate and make LessonListViewController the root View

--- LessonsApp/SceneDelegate.swift
+++ LessonsApp/SceneDelegate.swift
@@ -10,8 +10,20 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    private let navigationController = UINavigationController()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        navigationController.setViewControllers([makeRootViewController()], animated: false)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    func makeRootViewController() -> LessonsListViewController {
+        let vc = LessonsListViewController()
+        return vc
     }
 }
