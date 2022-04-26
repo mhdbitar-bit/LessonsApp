b136f1ad88b9e602283d0c95560bf2ee724a66c6
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-04-26 19:31:00 +0300

    Move LessonTableViewCell to it's own file

---
+++ LessonsApp/Presentation/Extensions/UIColor+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Ext.swift
+//  LessonsApp
+//
+//  Created by Mohammad Bitar on 4/26/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static let backgroundColor = UIColor(named: "BackgroundColor")
+}
--- LessonsApp/Presentation/LessonsListViewController.swift
+++ LessonsApp/Presentation/LessonsListViewController.swift
@@ -10,8 +10,32 @@ import Combine
 
 final class LessonsListViewController: UITableViewController {
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Lessons"
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .backgroundColor
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        view.backgroundColor = .backgroundColor
+        tableView.backgroundColor = .backgroundColor
+        
         
         tableView.register(LessonTableViewCell.self, forCellReuseIdentifier: LessonTableViewCell.identifier)
     }
@@ -29,72 +53,14 @@ extension LessonsListViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: LessonTableViewCell.identifier, for: indexPath)
-//        cell.lesson =
-        cell.accessoryType = .disclosureIndicator
+        let image = UIImage(systemName: "chevron.forward")
+        let disclosureIndicator = UIImageView(frame:CGRect(x: 0, y: 0, width: image?.size.width ?? 0, height: image?.size.height ?? 0))
+        disclosureIndicator.image = image
+        cell.accessoryView = disclosureIndicator
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
-}
-
-final class LessonTableViewCell: UITableViewCell {
-    static let identifier = "LessonTableViewCell"
-    
-    private let lessonImage: UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "test"))
-        imageView.contentMode = .scaleAspectFit
-        imageView.clipsToBounds = true
-        return imageView
-    }()
-    
-    private let lessonNameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "The Key To Success In iPhone Photography"
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        return label
-    }()
-  
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        contentView.addSubview(lessonImage)
-        contentView.addSubview(lessonNameLabel)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        lessonImage.anchor(
-            top: topAnchor,
-            left: leftAnchor,
-            bottom: bottomAnchor,
-            right: nil,
-            paddingTop: 5,
-            paddingLeft: 5,
-            paddingBottom: 5,
-            paddingRight: 0,
-            width: 90,
-            height: 0
-        )
-        
-        lessonNameLabel.anchor(
-            top: topAnchor,
-            left: lessonImage.rightAnchor,
-            bottom: bottomAnchor,
-            right: rightAnchor,
-            paddingTop: 5,
-            paddingLeft: 10,
-            paddingBottom: 0,
-            paddingRight: 0,
-            width: frame.size.width,
-            height: 0
-        )
+        return 80
     }
 }
