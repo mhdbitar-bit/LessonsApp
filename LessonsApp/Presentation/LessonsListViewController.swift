a58a15051e8bdfc18f81355b25dc8f22c3843190
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-04-26 18:36:44 +0300

    Build LessonTableViewCell programmatically

--- LessonsApp/Presentation/LessonsListViewController.swift
+++ LessonsApp/Presentation/LessonsListViewController.swift
@@ -13,10 +13,11 @@ final class LessonsListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(LessonTableViewCell.self, forCellReuseIdentifier: LessonTableViewCell.identifier)
     }
 }
 
-extension LessonsViewController {
+extension LessonsListViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -27,6 +28,73 @@ extension LessonsViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: LessonTableViewCell.identifier, for: indexPath)
+//        cell.lesson =
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+final class LessonTableViewCell: UITableViewCell {
+    static let identifier = "LessonTableViewCell"
+    
+    private let lessonImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "test"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let lessonNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "The Key To Success In iPhone Photography"
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        return label
+    }()
+  
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(lessonImage)
+        contentView.addSubview(lessonNameLabel)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        lessonImage.anchor(
+            top: topAnchor,
+            left: leftAnchor,
+            bottom: bottomAnchor,
+            right: nil,
+            paddingTop: 5,
+            paddingLeft: 5,
+            paddingBottom: 5,
+            paddingRight: 0,
+            width: 90,
+            height: 0
+        )
+        
+        lessonNameLabel.anchor(
+            top: topAnchor,
+            left: lessonImage.rightAnchor,
+            bottom: bottomAnchor,
+            right: rightAnchor,
+            paddingTop: 5,
+            paddingLeft: 10,
+            paddingBottom: 0,
+            paddingRight: 0,
+            width: frame.size.width,
+            height: 0
+        )
     }
 }
