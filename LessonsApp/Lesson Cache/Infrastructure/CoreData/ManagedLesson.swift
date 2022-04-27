8ad007e9a52f869e3285d3b7afb19a96bd9eeb2d
Mohammad Bitar <eng.mhd.bitar@gmail.com>
2022-04-27 20:50:12 +0300

    Add imageData attribute to the managed object

--- LessonsApp/Lesson Cache/Infrastructure/CoreData/ManagedLesson.swift
+++ LessonsApp/Lesson Cache/Infrastructure/CoreData/ManagedLesson.swift
@@ -14,6 +14,7 @@ class ManagedLesson: NSManagedObject {
     @NSManaged var lessonDescription: String
     @NSManaged var thumbnailUrl: URL
     @NSManaged var videoUrl: URL
+    @NSManaged var imageData: Data?
 }
 
 extension ManagedLesson {
